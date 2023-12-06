5a74f8b6f537b5103e538844a8a108aaa8b5e134
lhj50 <lhj50@DESKTOP-HUDC93C>
2023-12-06 15:43:55 +0900

    project3 ver.1.0

---
+++ project3/src/project3/code/HPButton.java
@@ -0,0 +1,19 @@
+package project3.code;
+
+import javax.swing.JButton;
+
+public class HPButton {
+	
+	JButton jButton;
+
+	public HPButton() {
+		super();
+	}
+
+	public HPButton(JButton jButton) {
+		this.jButton = jButton;
+	}
+
+}
+
+
